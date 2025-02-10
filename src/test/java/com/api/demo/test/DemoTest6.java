08f55557d288eef0e0bae5f92eb95019ddf1b2eb
jhashankar92 <102036107+jhashankar92@users.noreply.github.com>
2025-02-10 18:50:57 +0530

    C:\Data Drive\SeleniumWorkSpace\API Automation with Framework\CoreApiAutomationFW\src\test\java\com\api\demo\test\DemoTest6.javi print message

--- src/test/java/com/api/demo/test/DemoTest6.java
+++ src/test/java/com/api/demo/test/DemoTest6.java
@@ -14,7 +14,7 @@ public class DemoTest6 {
 	    		 break;
 	    	 }
 	     }
-	    		 System.out.println(allEven);	    	 
+	    		 System.out.println("Print the Output : " +allEven);	    	 
 	     	
 	}
 }
