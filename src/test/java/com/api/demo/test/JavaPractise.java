250f890213cb58c2fe3219d7a22012ed5235832f
jhashankar92 <102036107+jhashankar92@users.noreply.github.com>
2025-02-13 17:55:53 +0530

    Deletion of DemoTest7 Java Class File and Addtion of JavaPractiseFile

---
+++ src/test/java/com/api/demo/test/JavaPractise.java
@@ -0,0 +1,16 @@
+package com.api.demo.test;
+
+public class JavaPractise {
+
+	public static void main(String[] args) {
+
+		String str="Shankar Jha World";
+		StringBuffer sb=new StringBuffer(str);
+		sb.reverse();
+		System.out.println(sb);
+		
+		
+		
+	}
+
+}
