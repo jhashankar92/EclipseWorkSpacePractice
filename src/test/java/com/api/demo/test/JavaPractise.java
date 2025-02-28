beb5959f8d789818333673cd1b7a8460768d3ab5
jhashankar92 <102036107+jhashankar92@users.noreply.github.com>
2025-02-28 13:16:52 +0530

    code update for java practise

--- src/test/java/com/api/demo/test/JavaPractise.java
+++ src/test/java/com/api/demo/test/JavaPractise.java
@@ -10,21 +10,10 @@ public class JavaPractise {
 
 	public static void main(String[] args) {
 
-		String str = "KEYUR RATHOD";
-
-		LinkedHashSet<Character>  distinctCharacter=new LinkedHashSet<Character>();
 		
-		for(int i=0;i<str.length();i++) {
-			char ch=str.charAt(i);
-			if (ch!=' ') {
-			distinctCharacter.add(ch);			
-		}
-		}
-		System.out.println("Remove Duplicates Characters");
 		
-		for (char k:distinctCharacter) {
-			System.out.println(k);
-		}
+		
+		
 
 	}
 }
